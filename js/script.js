d9af4aa09b159024d1d3c787c75d39e4e63567f3
dbasumatary <basudhondip@gmail.com>
2023-04-06 06:21:15 +0530

    Added: Event listener on salary range to display value

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-//UC4 - specifying the value for salary
+//UC8 - Adding event listener for salary
 const salaryValue = document.querySelector('.salary-output');
 const salaryInputRange = document.querySelector('#salary');
 salaryInputRange.addEventListener("input",(event) => {
-    //salaryValue.textContent = event.target.value;
     salaryValue.textContent = salaryInputRange.value;
 });
